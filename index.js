8a1e3f199bd94707303f746ccc6f3e8f6f1d0033
sevketk <sevketk@yahoo.com>
2022-02-02 22:34:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ let map, infoWindow;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 6,
+    
+    zoom: 14,
   });
   infoWindow = new google.maps.InfoWindow();
 
